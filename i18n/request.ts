3ce865e4be1453cd6b1462a1de124a4c701af115
Simon Wilmes <wilmessimon@gmail.com>
2025-11-19 00:47:20 +0100

    Revert: Fix i18n config to return locale properly

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({ locale }) => {
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
 
